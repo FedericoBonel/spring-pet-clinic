402883f977bf346f9203103a596cdddac7d5f665
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-02-20 13:38:13 +0900

    Initial Project Addition

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-pet-clinic
+
 Basic Spring Framework Pet Clinic Application to get a hold of Spring concepts
---
+++ src/test/java/com/fedebonel/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedebonel.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
