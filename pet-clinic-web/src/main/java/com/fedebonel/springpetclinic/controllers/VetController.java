04d86e23a095b4d6987b523a2482c71c606dddc2
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-02-22 16:11:52 +0900

    Added the controller for the vet index page, and the vet index page itself. Closes #7

---
+++ pet-clinic-web/src/main/java/com/fedebonel/springpetclinic/controllers/VetController.java
@@ -0,0 +1,17 @@
+package com.fedebonel.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller for the vets
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
