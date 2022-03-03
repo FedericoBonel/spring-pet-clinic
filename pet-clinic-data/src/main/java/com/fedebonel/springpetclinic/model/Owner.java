6142d1fe1710e614b440bc7ddae7187659c0d28d
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-03-03 16:50:17 +0900

    Implemented the Visit POJO, Pet and Pet types were already created. Closes #27

--- pet-clinic-data/src/main/java/com/fedebonel/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/fedebonel/springpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package com.fedebonel.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * POJO For owners related to vet
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
