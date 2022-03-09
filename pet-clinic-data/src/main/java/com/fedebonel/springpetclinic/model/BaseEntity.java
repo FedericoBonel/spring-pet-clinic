0a4e00fd716c49f52f33d7bfa02d537330953533
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-03-10 00:06:47 +0900

    Implemented the base entity with MappedSuperClass. Closes #36

--- pet-clinic-data/src/main/java/com/fedebonel/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/fedebonel/springpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.fedebonel.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Base Entity POJO
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
