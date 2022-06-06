e3a6d830fc4cbbb4c391e4f6cb20850c4e3c5c24
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-06-06 16:18:37 +0900

    Added functionality to add and update owners. Closes #56

--- pet-clinic-web/src/main/java/com/fedebonel/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/fedebonel/springpetclinic/controllers/OwnerController.java
@@ -6,12 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -91,5 +89,41 @@ public class OwnerController {
             model.addAttribute("listOwners", owners);
             return "owners/ownersList";
         }
+    }
+
+    @GetMapping("/new")
+    public String createOwnerForm(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/new")
+    public String createOwner(@Valid Owner owner, BindingResult result) {
+        // If there were any errors show again the form because the owner is invalid
+        if (result.hasErrors()) {
+            return "owners/createOrUpdateOwnerForm";
+        }
+
+        Owner savedOwner = ownerService.save(owner);
+        return "redirect:/owners/" + savedOwner.getId();
+    }
+
+    @GetMapping("/{ownerId}/edit")
+    public String editOwnerForm(@PathVariable Long ownerId, Model model) {
+        model.addAttribute("owner", ownerService.findById(ownerId));
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/{ownerId}/edit")
+    public String editOwner(@PathVariable Long ownerId, @Valid Owner owner, BindingResult result) {
+        // If there were any errors show again the form because the owner is invalid
+        if (result.hasErrors()) {
+            return "owners/createOrUpdateOwnerForm";
+        }
+        // The id is is not being binded so we need to explicitly set it
+        owner.setId(ownerId);
+
+        Owner savedOwner = ownerService.save(owner);
+        return "redirect:/owners/" + savedOwner.getId();
     }
 }
